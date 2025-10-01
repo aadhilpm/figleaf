22c80f10a0412da4023c7a7bac6af9167070fc16
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2025-10-01 07:30:05 +0000

    fix: qatar phone condition in employee and contact

--- figleaf/overrides/contact/custom_contact.py
+++ figleaf/overrides/contact/custom_contact.py
@@ -4,5 +4,15 @@ from frappe.contacts.doctype.contact.contact import Contact
 
 class CustomContact(Contact):
     def before_save(self):
-        qatar_phone_condition = "+974-"+(self.mobile_no)
-        frappe.utils.validate_phone_number_with_country_code(qatar_phone_condition, "mobile_no") 
+        if not self.mobile_no:
+            return
+        if not self.mobile_no.startswith("+") or self.mobile_no.startswith("+974"):
+            # Determine Qatar phone format
+            qatar_phone_condition = (
+                self.mobile_no if self.mobile_no.startswith("+974")
+                else "+974-" + self.mobile_no
+            )
+            # Validate the number with country code
+            if frappe.utils.validate_phone_number_with_country_code(qatar_phone_condition, "mobile_no"):
+                # Update mobile_no with validated Qatar format
+                self.mobile_no = qatar_phone_condition
--- figleaf/overrides/employee/custom_employee.py
+++ figleaf/overrides/employee/custom_employee.py
@@ -3,5 +3,17 @@ from erpnext.setup.doctype.employee.employee import Employee
 
 class CustomEmployee(Employee):
     def before_save(self):
-        qatar_phone_condition = "+974-"+(self.cell_number)
-        frappe.utils.validate_phone_number_with_country_code(qatar_phone_condition, "cell_number")
+        if not self.cell_number:
+            return
+
+        # Determine Qatar phone format
+        if not self.cell_number.startswith("+") or self.cell_number.startswith("+974"):
+            qatar_phone_condition = (
+                self.cell_number if self.cell_number.startswith("+974") 
+                else "+974-" + self.cell_number
+            )
+
+            # Validate the number with country code
+            if frappe.utils.validate_phone_number_with_country_code(qatar_phone_condition, "cell_number"):
+                # Update cell_number with validated Qatar format
+                self.cell_number = qatar_phone_condition
