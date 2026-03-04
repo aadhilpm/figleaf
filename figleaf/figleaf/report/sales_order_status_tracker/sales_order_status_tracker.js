64569350dc27f90a1a8c45f8d4d5420b42c831d2
Aadhil <36843795+aadhilpm@users.noreply.github.com>
2026-03-04 15:07:20 +0300

    fix: simplify JS - remove formatter and status filter

--- figleaf/figleaf/report/sales_order_status_tracker/sales_order_status_tracker.js
+++ figleaf/figleaf/report/sales_order_status_tracker/sales_order_status_tracker.js
@@ -8,101 +8,43 @@ frappe.query_reports["Sales Order Status Tracker"] = {
 			label: __("Company"),
 			fieldtype: "Link",
 			options: "Company",
-			default: frappe.defaults.get_default("company"),
+			default: frappe.defaults.get_default("company")
 		},
 		{
 			fieldname: "from_date",
 			label: __("From Date"),
 			fieldtype: "Date",
-			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
 		},
 		{
 			fieldname: "to_date",
 			label: __("To Date"),
 			fieldtype: "Date",
-			default: frappe.datetime.get_today(),
+			default: frappe.datetime.get_today()
 		},
 		{
 			fieldname: "customer",
 			label: __("Customer"),
 			fieldtype: "Link",
-			options: "Customer",
+			options: "Customer"
 		},
 		{
 			fieldname: "sales_order",
 			label: __("Sales Order"),
 			fieldtype: "Link",
-			options: "Sales Order",
-			get_query: function () {
-				return {
-					filters: {
-						docstatus: 1,
-					},
-				};
-			},
+			options: "Sales Order"
 		},
 		{
 			fieldname: "item_code",
 			label: __("Item Code"),
 			fieldtype: "Link",
-			options: "Item",
-		},
-		{
-			fieldname: "status",
-			label: __("Status"),
-			fieldtype: "MultiSelectList",
-			get_data: function (txt) {
-				let status = [
-					"Draft",
-					"To Deliver and Bill",
-					"To Bill",
-					"To Deliver",
-					"Completed",
-					"Cancelled",
-					"Closed",
-				];
-				let options = [];
-				for (let option of status) {
-					options.push({
-						value: option,
-						label: __(option),
-						description: "",
-					});
-				}
-				return options;
-			},
+			options: "Item"
 		},
 		{
 			fieldname: "sales_person",
 			label: __("Sales Person"),
 			fieldtype: "Link",
-			options: "Sales Person",
-		},
-	],
-
-	formatter: function (value, row, column, data, default_formatter) {
-		value = default_formatter(value, row, column, data);
-
-		if (column.fieldname == "order_status" && data) {
-			if (data.order_status == "Completed") {
-				value = "<span style='color:green; font-weight:bold;'>" + value + "</span>";
-			} else if (data.order_status == "To Deliver and Bill") {
-				value = "<span style='color:orange;'>" + value + "</span>";
-			} else if (data.order_status == "Cancelled" || data.order_status == "Closed") {
-				value = "<span style='color:red;'>" + value + "</span>";
-			}
+			options: "Sales Person"
 		}
-
-		if (column.fieldname == "work_order_status" && data && data.work_order_status) {
-			if (data.work_order_status == "Completed") {
-				value = "<span style='color:green; font-weight:bold;'>" + value + "</span>";
-			} else if (data.work_order_status == "In Process") {
-				value = "<span style='color:blue;'>" + value + "</span>";
-			} else if (data.work_order_status == "Not Started") {
-				value = "<span style='color:orange;'>" + value + "</span>";
-			}
-		}
-
-		return value;
-	},
+	]
 };
